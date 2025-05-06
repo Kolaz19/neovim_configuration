f094910f53afec67c8c06718379d858c8ffd998d
Nikolas <etc.nikolas@gmail.com>
2025-05-06 07:57:22 +0200

    Lazy load luasnip plugin

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,41 +1,41 @@
-local ls = require("luasnip")
-local extras = require("luasnip.extras")
 return {
 	'L3MON4D3/LuaSnip',
-	config = true,
 	lazy = true,
-	snip_env = {
-		--Define shortcuts for snippet files here
-		--Otherwise we would have to define them in every snippet file
-		--LuaSnip does use them automatically
-		s = ls.snippet,
-		sn = ls.snippet_node,
-		isn = ls.indent_snippet_node,
-		t = ls.text_node,
-		i = ls.insert_node,
-		f = ls.function_node,
-		c = ls.choice_node,
-		d = ls.dynamic_node,
-		r = ls.restore_node,
-		events = require("luasnip.util.events"),
-		ai = require("luasnip.nodes.absolute_indexer"),
-		extras = extras,
-		l = extras.lambda,
-		rep = extras.rep,
-		p = extras.partial,
-		m = extras.match,
-		n = extras.nonempty,
-		dl = extras.dynamic_lambda,
-		fmt = require("luasnip.extras.fmt").fmt,
-		fmta = require("luasnip.extras.fmt").fmta,
-		conds = require("luasnip.extras.expand_conditions"),
-		postfix = require("luasnip.extras.postfix").postfix,
-		types = require("luasnip.util.types"),
-		parse = require("luasnip.util.parser").parse_snippet,
-		ms = ls.multi_snippet,
-		k = require("luasnip.nodes.key_indexer").new_key
-	},
-	init = function()
+	config = function()
+		require("luasnip").setup {
+			snip_env = {
+				--Define shortcuts for snippet files here
+				--Otherwise we would have to define them in every snippet file
+				--LuaSnip does use them automatically
+				ls = require("luasnip"),
+				s = require("luasnip").snippet,
+				sn = require("luasnip").snippet_node,
+				isn = require("luasnip").indent_snippet_node,
+				t = require("luasnip").text_node,
+				i = require("luasnip").insert_node,
+				f = require("luasnip").function_node,
+				c = require("luasnip").choice_node,
+				d = require("luasnip").dynamic_node,
+				r = require("luasnip").restore_node,
+				events = require("luasnip.util.events"),
+				ai = require("luasnip.nodes.absolute_indexer"),
+				extras = require("luasnip.extras"),
+				l = require("luasnip.extras").lambda,
+				rep = require("luasnip.extras").rep,
+				p = require("luasnip.extras").partial,
+				m = require("luasnip.extras").match,
+				n = require("luasnip.extras").nonempty,
+				dl = require("luasnip.extras").dynamic_lambda,
+				fmt = require("luasnip.extras.fmt").fmt,
+				fmta = require("luasnip.extras.fmt").fmta,
+				conds = require("luasnip.extras.expand_conditions"),
+				postfix = require("luasnip.extras.postfix").postfix,
+				types = require("luasnip.util.types"),
+				parse = require("luasnip.util.parser").parse_snippet,
+				ms = require("luasnip").multi_snippet,
+				k = require("luasnip.nodes.key_indexer").new_key
+			}
+		}
 		--Load all snippets
 		local configDir = vim.fn.stdpath('config')
 		require("luasnip.loaders.from_lua").load({ paths = configDir .. "/lua/plugins/snippets" })
